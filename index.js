d74cd93ae7866ea68520b8cc94ec6554162cdfff
LordAldi <aldianugra09@gmail.com>
2020-07-18 08:28:26 +0700

    rest api, add restructure and connect to db

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ mongoose
   .connect(config.get("mongodb.server"), {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false,
   })
   .then(() => debug("Connected to MongoDb"))
   .catch((err) => console.error("Could not connect to mongoDB..."));
@@ -22,88 +23,11 @@ app.use(express.json());
 app.use(helmet());
 app.use("/api/tickets", tickets);
 app.use("/", home);
-const ticketSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  project: String,
-  created: { type: Date, default: Date.now },
-  updated: { type: Date, default: Date.now },
-  assigenedTo: [String],
-  attachment: [String],
-  priority: String,
-  type: String,
-  owner: String,
-  history: [String],
-});
-const Ticket = mongoose.model("ticket", ticketSchema);
 
-async function createTicket() {
-  const ticket = new Ticket({
-    title: "world",
-    description: "this is",
-    project: "meeaaww",
-    assigenedTo: ["usera", "user b"],
-    attachment: [],
-    priority: "high",
-    type: "feature request",
-    owner: "usera",
-    history: [""],
-  });
-
-  const result = await ticket.save();
-  debug(result);
-}
-async function getTickets() {
-  const pageNumber = 1;
-  const pageSize = 10;
-  const tickets = await Ticket.find({ priority: "high" })
-    .skip((pageNumber - 1) * pageSize)
-    .limit(pageSize)
-    .sort({ title: 1 }) //1 asc -1 dsc
-    .select({ title: 1, description: 1, project: 1 });
-  // .countDocuments();
-  debug(tickets);
-}
-async function updateTicket(id) {
-  // const ticket = await Ticket.findById(id);
-  // if (!ticket) return;
-  // ticket.set({
-  //   title: "new title",
-  //   project: "new project",
-  // });
-  // const result = await ticket.save();
-
-  // const result = await Ticket.updateOne(
-  //   { _id: id },
-  //   {
-  //     $set: {
-  //       title: "new title new",
-  //       project: "new project new",
-  //     },
-  //   }
-  // );
-
-  const ticket = await Ticket.findByIdAndUpdate(
-    id,
-    {
-      $set: {
-        title: "new2",
-        project: "new2  new",
-      },
-    },
-    { new: true }
-  );
-
-  debug(ticket);
-}
-async function removeTicket(id) {
-  const result = await Ticket.deleteOne({ _id: id });
-  debug(result);
-}
 // createTicket();
 // getTickets();
 // updateTicket("5f0fb91b5ac9095664ea0368");
-removeTicket("5f0fb91b5ac9095664ea0368");
+// removeTicket("5f0fb91b5ac9095664ea0368");
 
 if (app.get("env") === "development") {
   app.use(morgan("tiny"));
--- routes/tickets.js
+++ routes/tickets.js
@@ -1,119 +1,166 @@
 const express = require("express");
 const router = express.Router();
 const Joi = require("joi");
+const mongoose = require("mongoose");
 
-const tickets = [
-  {
-    id: 1,
-    title: "hallo",
-    description: "world",
-    project: "test project",
-    created: "",
-    updated: "",
-    assigenedTo: "",
-    attachment: "",
-    priority: "low, medium,high, critical",
-    type: "error report, feature request, service request, other",
-    owner: "",
-    history: "",
+const ticketSchema = new mongoose.Schema({
+  title: { type: String, required: true, minlength: 3 },
+  description: String,
+  project: { type: String, required: true },
+  created: { type: Date, default: Date.now },
+  updated: { type: Date, default: Date.now },
+  assigenedTo: [String],
+  attachment: [String],
+  priority: {
+    type: String,
+    required: true,
+    enum: ["low", "medium", "high", "critical"],
+    lowercase: true,
   },
-  {
-    id: 2,
-    title: "hallo2",
-    description: "world 2",
-    project: "test project 2",
-    created: "",
-    updated: "",
-    assigenedTo: "",
-    attachment: "",
-    priority: "low, medium,high, critical",
-    type: "error report, feature request, service request, other",
-    owner: "",
-    history: "",
+  type: {
+    type: String,
+    required: true,
+    enum: ["error report", "feature request", "service request", "other"],
+    lowercase: true,
   },
-  {
-    id: 3,
-    title: "hallo3",
-    description: "world 3",
-    project: "test project 2",
-    created: "",
-    updated: "",
-    assigenedTo: "",
-    attachment: "",
-    priority: "low, medium,high, critical",
-    type: "error report, feature request, service request, other",
-    owner: "",
-    history: "",
-  },
-  {
-    id: 4,
-    title: "hallo4",
-    description: "world 4",
-    project: "test project 2",
-    created: "",
-    updated: "",
-    assigenedTo: "",
-    attachment: "",
-    priority: "low, medium,high, critical",
-    type: "error report, feature request, service request, other",
-    owner: "",
-    history: "",
-  },
-];
+  owner: { type: String, required: true },
+  history: [String],
+});
+const Ticket = mongoose.model("ticket", ticketSchema);
 
-router.get("/", (req, res) => {
+async function createTicket(data) {
+  const {
+    title,
+    description,
+    project,
+    assigenedTo,
+    attachment,
+    priority,
+    type,
+    owner,
+  } = data;
+  const ticket = new Ticket({
+    title: title,
+    description: description,
+    project: project,
+    assigenedTo: assigenedTo,
+    attachment: attachment,
+    priority: priority,
+    type: type,
+    owner: owner,
+  });
+  return ticket;
+}
+async function getTickets(query) {
+  const pageNumber = query.pn;
+  const pageSize = query.pz;
+  const tickets = await Ticket.find()
+    .skip((pageNumber - 1) * pageSize)
+    .limit(pageSize)
+    .sort({ title: 1 }); //1 asc -1 dsc
+  // .select({ title: 1, description: 1, project: 1 });
+  // .countDocuments();
+  return tickets;
+}
+async function getTicketById(id) {
+  const ticket = await Ticket.findById(id);
+  return ticket;
+}
+async function updateTicket(id, data) {
+  const ticket = await Ticket.findByIdAndUpdate(
+    id,
+    {
+      $set: {
+        ...data,
+      },
+    },
+    { new: true }
+  );
+
+  return ticket;
+}
+async function removeTicket(id) {
+  const ticket = await Ticket.findByIdAndRemove(id);
+  return ticket;
+}
+
+router.get("/", async (req, res) => {
+  const tickets = await getTickets(req.query);
   res.send(tickets);
 });
-router.get("/:id", (req, res) => {
-  const ticket = tickets.find((t) => t.id === parseInt(req.params.id));
+router.get("/:id", async (req, res) => {
+  const ticket = await getTicketById(req.params.id);
   if (!ticket)
     return res.status(404).send("Ticket with the given ID was not found");
   res.send(ticket);
 });
-router.post("/", (req, res) => {
-  const { error } = validateTicket(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-  const ticket = {
-    ...tickets[0],
-    id: tickets.length + 1,
-    title: req.body.title,
-    description: req.body.description,
-    project: `project${tickets.length + 1}`,
-  };
-  tickets.push(ticket);
-  res.send(ticket);
-});
-
-router.put("/:id", (req, res) => {
-  const ticket = tickets.find((t) => t.id === parseInt(req.params.id));
-  if (!ticket)
-    return res.status(404).send("Ticket with the given ID was not found");
-
+router.post("/", async (req, res) => {
   const { error } = validateTicket(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  ticket.title = req.body.title;
-  ticket.description = req.body.description;
-  res.send(ticket);
+  let ticket = await createTicket(req.body);
+  try {
+    ticket = await ticket.save();
+    res.send(ticket);
+  } catch (ex) {
+    for (field in ex.errors) res.send(ex.errors[field].message);
+  }
 });
 
-router.delete("/:id", (req, res) => {
-  const ticket = tickets.find((t) => t.id === parseInt(req.params.id));
+router.put("/:id", async (req, res) => {
+  const { error } = validateUpdateTicket(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const ticket = await updateTicket(req.params.id, req.body);
+
   if (!ticket)
     return res.status(404).send("Ticket with the given ID was not found");
 
-  const index = tickets.indexOf(ticket);
-  tickets.splice(index, 1);
+  try {
+    console.log("try");
+    res.send(ticket);
+  } catch (ex) {
+    for (field in ex.errors) res.send(ex.errors[field].message);
+  }
+});
 
-  res.send(ticket);
+router.delete("/:id", async (req, res) => {
+  const ticket = await removeTicket(req.params.id);
+  if (!ticket)
+    return res.status(404).send("Ticket with the given ID was not found");
+
+  try {
+    ticket = await ticket.save();
+    res.send(ticket);
+  } catch (ex) {
+    for (field in ex.errors) res.send(ex.errors[field].message);
+  }
 });
 
 validateTicket = (ticket) => {
   const schema = {
     title: Joi.string().min(3).required(),
-    description: Joi.string().min(3).required(),
+    description: Joi.string(),
+    project: Joi.string().min(3).required(),
+    assigenedTo: Joi.array(),
+    attachment: Joi.array(),
+    priority: Joi.string().required(),
+    type: Joi.string().required(),
+    owner: Joi.string().required(),
   };
   return Joi.validate(ticket, schema);
 };
-
+validateUpdateTicket = (ticket) => {
+  const schema = {
+    title: Joi.string().min(3),
+    description: Joi.string(),
+    project: Joi.string().min(3),
+    assigenedTo: Joi.array(),
+    attachment: Joi.array(),
+    priority: Joi.string(),
+    type: Joi.string(),
+    owner: Joi.string(),
+  };
+  return Joi.validate(ticket, schema);
+};
 module.exports = router;
